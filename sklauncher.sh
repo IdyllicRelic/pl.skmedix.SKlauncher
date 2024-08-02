fc059957f91db48c79592be69847cec4bc2717f2
Reizij <147831357+Reizij@users.noreply.github.com>
2024-08-02 11:21:14 +0530

    Initial Commit

---
+++ sklauncher.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+for i in {0..9}; do
+    test -S $XDG_RUNTIME_DIR/discord-ipc-$i || ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
+done
+
+java -jar /app/extra/SKlauncher-3.2.9.jar
+
