4bf665c13faea8afdeb82d6366bfe3a912afdead
Reizij <147831357+Reizij@users.noreply.github.com>
2024-08-31 17:02:47 +0530

    Updated to version 3.2.10

--- sklauncher.sh
+++ sklauncher.sh
@@ -4,5 +4,5 @@ for i in {0..9}; do
     test -S $XDG_RUNTIME_DIR/discord-ipc-$i || ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
 done
 
-java -jar /app/extra/SKlauncher-3.2.9.jar
+java -jar /app/extra/SKlauncher-3.2.10.jar
 
